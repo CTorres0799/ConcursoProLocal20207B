5afca4db8bcad09d7672cd0ae2b062938675f62c
CTorres0799 <ctorres340@accitesz.com>
2020-12-03 16:35:05 -0600

    creacion tablas by CT
    03/12

--- templates/ConcursoPro20207B.sql
+++ templates/ConcursoPro20207B.sql
@@ -1,22 +1,67 @@
 create database ConcursoPro20207B;
 use ConcursoPro20207B
 
-/* -----------------------♥Cristian♥----------------------------  */
+/* -----------------------Cristian----------------------------  */
 /*==============================================================*/
 /* Table: Categorias                                               */
 /*==============================================================*/
+
+create table Categorias 
+( 
+idCategoria int auto_increment not null,
+nombre varchar(30),
+semestreLimite char(2),
+idProPue int,
+   constraint pk_categorias primary key (idCategoria),
+   constraint uk_idProPue_categorias unique(idProPue) 
+);
 
 /*==============================================================*/
 /* Table: Alumnos                                               */
 /*==============================================================*/
 
+create table Alumnos 
+(
+NoControl int not null,
+semestre char(2),
+idUsuario int not null,
+idCarrera int not null,
+idProRes int not null,
+	constraint pk_alumnos primary key (NoControl),
+    constraint uk_idCarrera_alumnos unique(idCarrera)
+);
+
 /*==============================================================*/
-/* Table: ProblemasPropuestos                                               */
+/* Table: ProblemasPropuestos                                   */
 /*==============================================================*/
+
+create table ProblemasPropuestos 
+(
+idProPue int auto_increment not null,
+globo varchar(15),
+idProblema int not null,
+idEdicion int not null,
+idCategoria int not null,
+	constraint pk_problemaspropuestos primary key (idProPue),
+    constraint uk_idCarrera_problemaspropuestos unique(idProblema)
+);
 
 /*==============================================================*/
 /* Table: Docentes                                               */
 /*==============================================================*/
+
+create table Docentes
+(
+idDocente int auto_increment not null,
+escolaridad varchar(4),
+especialidad varchar(40),
+cedula int (8),
+idCarrera int not null,
+idUsuario int not null,
+noControl int not null,
+	constraint pk_docentes primary key (idDocente),
+    constraint uk_cedula_docentes unique(cedula)
+);
 
 /* -------------------------♠Pedro♠-----------------------------  */
 /*==============================================================*/
