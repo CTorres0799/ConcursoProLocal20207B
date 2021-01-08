d724741c2967e18a88fde30223a3f4196e5be2e9
CTorres0799 <ctorres340@accitesz.com>
2021-01-08 11:19:54 -0600

    CRUD BASICO FUNCIONANDO
    Tabla alumnos FUNCIONANDO

--- ConcursoProLocal20207B/app.py
+++ ConcursoProLocal20207B/app.py
@@ -1,51 +1,117 @@
+from _ast import alias
 from symbol import return_stmt
-from urllib import request
-
+#from typing import red
+from flask import request
 from Tools.scripts.make_ctype import method
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
-from modelo.models import db, Categoria
+from modelo.models import db
+from modelo.models import Categorias,Alumnos,PPropuestos,Docentes
+from setuptools.command.alias import alias
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://concursoprog_user:hola.123@localhost/ConcursoPro20207B'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:@cf0709-1415@localhost/ConcursoPro20207B'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 #db = SQLAlchemy(app)
 
 @app.route('/')
 def Index():
     return  'Hello World'
-
-@app.route('/add_alumno')
-def add_alumno():
-    return 'Añadir Contacto'
-
-@app.route('/editar')
-def editar_alumno():
-    return 'Editando Alumno'
-
-@app.route('/eliminar')
-def eliminar_alumno():
-    return 'Alumno Eliminado'
-
-#Inicio CRUD tabla Categorias
-
+#crud categorias
 @app.route('/categorias/new')
 def nuevaCategoria():
     return render_template('Categorias/nuevaCategoria.html')
-@app.route('/Categorias/save',methods=['POST'])
+@app.route('/categorias/guardar',methods=['POST'])
 def guardarCategoria():
-    categoria = Categoria()
-    #categoria.idCategoria = request.form['idCategoria']
-    #categoria.insertar()
-    categoria.nombre = request.form['nombre']
-    #categoria.insertar()
-    categoria.semestreLimite = request.form['semestreLimite']
-    #categoria.insertar()
-    #categoria.idProPue = request.form['idProPue']
-    categoria.insertar()
-    return 'Categoria Registrada con Exito'
-#FIN CRUD CATEGORIAS
+ categorias=Categorias()
+ categorias.Nombre=request.form['nombre']
+ categorias.SemestreLimite=request.form['semestreLimite']
+ categorias.idProPue=request.form['idProPue']
+ categorias.insertar()
+ return 'Categoria Registrada con Exito'
+#final
+
+#INICIO CRUD alumnos FUNCIONANDO
+@app.route('/alumnos/new')
+def nuevoAlumno():
+    return render_template('Alumnos/nuevoAlumno.html')
+@app.route('/alumnos/eliminar/<int:noControl>')
+def eliminarAlumno(noControl):
+    alucno=Alumnos()
+    alucno.noControl = noControl
+    alucno.eliminar()
+    return redirect(url_for('consultaGeneralAlumnos'))
+@app.route('/alumnos/guardar', methods=['POST'])
+def guardarAlumno():
+    alucnos=Alumnos()
+    alucnos.noControl=request.form ['noControl']
+    alucnos.semestre=request.form ['semestre']
+    alucnos.idUsuario=request.form ['idUsuario']
+    alucnos.idCarrera=request.form ['idCarrera']
+    alucnos.idProRes=request.form ['idProRes']
+    alucnos.insertar()
+    return redirect(url_for('consultaGeneralAlumnos'))
+@app.route('/Alumnos')
+def consultaGeneralAlumnos():
+    alucnos = Alumnos()
+    alucnosGeneral = alucnos.consultaGeneral()
+    return render_template('Alumnos/consultaGeneral.html',alucnosGeneral=alucnosGeneral)
+@app.route('/alumnos/<int:noControl>')
+def consultarAlumno(noControl):
+    alucnos = Alumnos()
+    alucnos.noControl=noControl
+    alucnos = alucnos.consultaIndividual()
+    return render_template('Alumnos/editarAlumno.html',alucnos=alucnos)
+@app.route('/alumnos/modificar', methods = ['POST'])
+def actualizarAlumno():
+    alucnos = Alumnos()
+    alucnos.noControl=request.form['noControl']
+    alucnos.semestre=request.form['semestre']
+    alucnos.idUsuario=request.form['idUsuario']
+    alucnos.idCarrera=request.form['idCarrera']
+    alucnos.idProRes=request.form['idProRes']
+    alucnos.actualizar()
+    return redirect(url_for('consultaGeneralAlumnos'))
+#FIN CRUD ALUMNOS FUNCIONANDO
+
+#crud ProblemasPropuestos
+@app.route('/pp/new')
+def nuevoPP():
+    return render_template('problemasPropuestos/nuevoPP.html')
+@app.route('/pp/guardar', methods=['POST'])
+def guardarPP():
+    pp = PPropuestos()
+    pp.idProPue=request.form['']
+    pp.globo.form['']
+    pp.idProblema.form['']
+    pp.idEdicion.form['']
+    pp.idCategoria.fomr['']
+    pp.insertar()
+    return 'Problema propuesto Añadido correctamente'
+#final
+#crud Docentes
+@app.route('/docentes/new')
+def nuevoDocente():
+    return render_template('Docentes/nuevoDocentes.html')
+@app.route('/docentes/guardar', methods=['POST'])
+def guardarDocente():
+    docente = Docentes()
+    docente.idDocente = request.form['idDocente']
+    docente.escolaridad = request.form['escolaridad']
+    docente.especialidad = request.form['especialidad']
+    docente.cedula = request.form['cedula']
+    docente.idCarrera = request.form['idCarrera']
+    docente.idUsuario = request.form['idUsuario']
+    docente.noControl = request.fomr['noControl']
+    docente.insert()
+    return 'Docentes agregado con exito'
+#final
+
 
 if __name__ == '__main__':
  db.init_app(app)
+
+with app.app_context():
+ #db.create_all()
+
  app.run(port = 3000, debug=True)
--- ConcursoProLocal20207B/modelo/models.py
+++ ConcursoProLocal20207B/modelo/models.py
@@ -4,15 +4,71 @@ from sqlalchemy import Column,Integer,String
 db = SQLAlchemy()
 
 #KITIAN
-class Categoria  (db.Model):
+class Categorias(db.Model):
     __tablename__='Categorias'
-    idCategoria = Column(Integer,primary_key=True)
-    nombre = Column(String,unique=True)
-    semestreLimite = Column(Integer)
-    idProPue = Column(Integer)
+    id = Column(Integer,primary_key=True)
+    Nombre = Column(String(30),unique=True)
+    SemestreLimite = Column(Integer)
+    IdProPue = Column(Integer)
     def insertar(self):
         db.session.add(self)
         db.session.commit()
     def consultaGeneral(self):
         categorias = self.query.all()
         return  categorias
+
+#INICIO ALUMNOS
+class Alumnos(db.Model):
+    __tablename__='Alumnos'
+    noControl = Column(Integer,primary_key=True)
+    semestre = Column(String(2))
+    idUsuario = Column(Integer)
+    idCarrera = Column(Integer)
+    idProRes = Column(Integer)
+    def insertar(self):
+        db.session.add(self)
+        db.session.commit()
+    def consultaGeneral(self):
+        Alumnos = self.query.all()
+        return  Alumnos
+    def actualizar(self):
+        db.session.merge(self)
+        db.session.commit()
+    def eliminar(self):
+        Alumnos = self.consultaIndividual()
+        db.session.delete(Alumnos)
+        db.session.commit()
+    def consultaIndividual(self):
+       Alumnos = self.query.get(self.noControl)
+       return Alumnos
+#FIN ALUMNOS
+
+class PPropuestos (db.Model):
+    __tablename__='ProblemasPropuestos'
+    idProPue = Column(Integer,primary_key=True)
+    globo = Column(String(15))
+    idProblema = Column(Integer)
+    idEdicion = Column(Integer)
+    idCategoria = Column(Integer)
+    def insertar(self):
+        db.session.add(self)
+        db.session.commit()
+    def consultaGeneral(self):
+        PP = self.query.all()
+        return PP
+class Docentes (db.Model):
+    __tablename__='Docentes'
+    idDocente = Column(Integer,primary_key=True)
+    escolaridad = Column(String(50))
+    especialidad = Column(String(50))
+    cedula = Column(Integer)
+    idCarrera = Column(Integer)
+    idUsuario = Column(Integer)
+    noControl = Column(Integer)
+    def insert(self):
+        db.session.add(self)
+        db.session.add()
+    def consultarGeneral(self):
+        docentes = self.query.all()
+        return docentes
+
